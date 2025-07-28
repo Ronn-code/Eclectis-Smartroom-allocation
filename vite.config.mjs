552c6e1f4082230895a3953c20aa49d26a17f3ae
ronn <ronnthecoder@gmail.com>
2025-07-28 15:59:20 +0300

    Fixed the routing

--- vite.config.mjs
+++ vite.config.mjs
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }) => {
         // },
       ]
     },
-    base: './',
+    base: '/',
     plugins: [react(), jsconfigPaths()],
     optimizeDeps: {
     include: ['react-dom/client']
